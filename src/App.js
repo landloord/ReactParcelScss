1b59154a37a90da3775ac4ae83dd4842e75e9c11
Landloord <agyabahopefaro@gmail.com>
2020-08-27 19:00:35 +0000

    Fixed errors in App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./App.scss";
 
 const App = () => {
-  return <h1>Hello, Welcome to your new project</h1>;
-};
+  return (<h1>Hello, Welcome to your new project</h1>);
+}
 
 export default App;
